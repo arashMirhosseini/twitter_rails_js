167aa453beaef98e0b9bee8e32b1ae0f733faf5f
arash <arash.mh01@gmail.com>
2019-09-07 18:02:29 -0700

    Add maximum characters counter for each tweets

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -198,6 +198,8 @@ const APIUtil = __webpack_require__(/*! ./api_util */ "./frontend/api_util.js");
 class TweetCompose {
   constructor() {
     this.$form = $("form.tweet-compose");
+    this.$input = this.$form.find('textarea[name=tweet\\[content\\]]');
+    this.$input.on('input', this.handleInput.bind(this));
     this.submit();
   }
   
@@ -223,9 +225,27 @@ class TweetCompose {
   handleSuccess(data) {
     this.clearInput();
     const $tweetsUl = $(this.$form.data('tweets-ul'));
-    const $li = $("<li></li>").text(data.content);
+    const $li = $("<li></li>");
+    this.addTweet($li, data);
     $tweetsUl.prepend($li);
-    
+  }
+
+  handleInput(event) {
+    const inputLength = this.$input.val().length;
+    this.$form.find(".chars-left").text(`${140 - inputLength} characters left`);
+  }
+
+  addTweet($li, data) {
+    const $div = $("<div></div>").addClass("tweet");
+    $li.append($div);
+    const $h3 = $("<h3></h3>").addClass("tweeter");
+    const $aUser = $("<a></a>").text(data.user.username);
+    $aUser.attr("href", "");
+    const $p = $("<p></p>").text(data.content);
+    $h3.append($aUser);
+    $div.append($h3);
+    $div.append($p);
+
   }
 
 }
--- frontend/tweet_compose.js
+++ frontend/tweet_compose.js
@@ -3,6 +3,8 @@ const APIUtil = require("./api_util");
 class TweetCompose {
   constructor() {
     this.$form = $("form.tweet-compose");
+    this.$input = this.$form.find('textarea[name=tweet\\[content\\]]');
+    this.$input.on('input', this.handleInput.bind(this));
     this.submit();
   }
   
@@ -28,9 +30,27 @@ class TweetCompose {
   handleSuccess(data) {
     this.clearInput();
     const $tweetsUl = $(this.$form.data('tweets-ul'));
-    const $li = $("<li></li>").text(data.content);
+    const $li = $("<li></li>");
+    this.addTweet($li, data);
     $tweetsUl.prepend($li);
-    
+  }
+
+  handleInput(event) {
+    const inputLength = this.$input.val().length;
+    this.$form.find(".chars-left").text(`${140 - inputLength} characters left`);
+  }
+
+  addTweet($li, data) {
+    const $div = $("<div></div>").addClass("tweet");
+    $li.append($div);
+    const $h3 = $("<h3></h3>").addClass("tweeter");
+    const $aUser = $("<a></a>").text(data.user.username);
+    $aUser.attr("href", "");
+    const $p = $("<p></p>").text(data.content);
+    $h3.append($aUser);
+    $div.append($h3);
+    $div.append($p);
+
   }
 
 }
